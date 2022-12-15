901c5cc778903d56f04e2a9f2364ad0ab46143ef
Veeramani4156 <veeramani4156@gmail.com>
2022-12-15 13:07:44 +0530

    import scanner class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
-
+import java.util.Scanner:
 public class Main {
+    Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
 }
